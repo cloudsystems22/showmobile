c4594c13ff59dbce02f2e655cd0f10e1bbc6c23a
cloudsystems22 <cloudsystems22@gmail.com>
2020-08-19 11:59:35 -0300

    Layout inicial

---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Show mobile || KLB</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/flex.css">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/pallet.css">
+</head>
+<body>
+    <div id='nav-bar'>
+        <div class='container'>
+            <div class='row'>
+                <div class='col-3 col-md-3 col-sm-3'>
+                    <a href="#!">
+                        <img src="image/logo-preto.png" alt="Show Mobile" class='logo'>
+                    </a>
+                </div>
+                <div class='col-6 col-md-6 col-sm-6'>
+                    <ul class='nav'>
+                        <li><a href="#!">Home</a></li>
+                        <li><a href="#!">Tarifas Móveis</a></li>
+                        <li><a href="#!">Nossos Artistas</a></li>
+                        <li><a href="#!">Sobre nós</a></li>
+                        <li><a href="#!">Contato</a></li>
+                    </ul>
+                </div>
+                <div class='col-3 col-md-3 col-sm-3'>
+                    <ul class='nav'>
+                        <li><a href="#!"><img src="ico/instagram.png" alt="" class='img-ico'></a></li>
+                        <li><a href="#!"><img src="ico/facebook.png" alt="" class='img-ico'></a></li>
+                        <li><a href="#!"><img src="ico/youtube.png" alt="" class='img-ico-youtube'></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id='banner'>
+        <div class='banner-fundo'>
+         
+        </div>
+    </div>
+    <div id='menu-icon'>
+        <!-- <ul class='menu-ico'>
+            <li><a href="#!" class='font-color-orange'><img src="ico/team.png" alt=""><br>Conheça a show</a></li>
+            <li><a href="#!"><img src="ico/monitor.png" alt=""><br>Planos</a></li>
+            <li><a href="#!"><img src="ico/singer.png" alt=""><br>Artistas</a></li>
+            <li><a href="#!"><img src="ico/marketing.png" alt=""><br>Novidades</a></li>
+            <li><a href="#!"><img src="ico/support.png" alt=""><br>Suporte</a></li>
+        </ul> -->
+        <div class='row' id='grupo-btn'>
+            <a href="#!" class='btn-menu-ico font-color-orange'><img src="ico/team.png" alt=""><br>Conheça a show</a>
+            <a href="#!" class='btn-menu-ico font-color-orange'><img src="ico/monitor.png" alt=""><br>Planos</a>
+            <a href="#!" class='btn-menu-ico font-color-orange'><img src="ico/singer.png" alt=""><br>Artistas</a>
+            <a href="#!" class='btn-menu-ico font-color-orange'><img src="ico/marketing.png" alt=""><br>Novidades</a>
+            <a href="#!" class='btn-menu-ico font-color-orange'><img src="ico/support.png" alt=""><br>Suporte</a>
+        </div>
+    </div>
+    <div id='text-apresent'>
+        <h3>Conheça nossas <span class='font-color-orange'>tarifas móveis</span> e aproveite a <span class='font-color-orange'>melhor conexão</span> com <span class='font-color-orange'>conteúdos exclusivos</span> do seus artistas preferidos <span class='font-color-orange'></span></h3>
+    </div>
+    <hr>
+    <div id='sessao-planos'>
+        <div class='fundo-planos'>
+            <div class='planos-group row'>
+                <a href="#!">
+                    <div class='plano plano-um'>
+                        <p class='font-color-orange span-plano25'>Ligações e SMS</p>
+                        <p class='font-color-orange span-plano40italico'>Ilimitados</p>
+                        <p class='font-color-orange '><span class='span-plano60'>5</span><span class='span-plano50'>Gb</span></p>
+                        <p class='font-color-orange'>R$<span class='span-plano50'>49</span>,90</p>
+                    </div>
+                </a>
+                <a href="#!">
+                    <div class='plano plano-dois'>
+                        <p class='font-color-orange span-plano25'>Ligações e SMS</p>
+                        <p class='font-color-orange span-plano40italico'>Ilimitados</p>
+                        <p class='font-color-orange span-plano50italico'><span class='span-plano60'>10</span><span class='span-plano50'>Gb</span></p>
+                        <p class='font-color-orange'>R$<span class='span-plano50'>59</span>,90</p>
+                    </div>
+                </a>
+                <a href="#!">
+                    <div class='plano plano-tres'>
+                        <p class='font-color-orange span-plano25'>Ligações e SMS</p>
+                        <p class='font-color-orange span-plano40italico'>Ilimitados</p>
+                        <p class='font-color-orange span-plano50italico'><span class='span-plano60'>15</span><span class='span-plano50'>Gb</span></p>
+                        <p class='font-color-orange'>R$<span class='span-plano50'>79</span>,90</p>
+                    </div>
+                </a>
+
+            </div>
+
+        </div>
+    </div>
+    <div id='nossos-artistas'>
+        <h2>Nossos <span class='font-color-orange'>Artistas</span></h2>
+        <div class='fundo-artistas'>
+            <div class='planos-group row'>
+                <a href="#!">
+                    <div class='plano'>
+                        <div class='plan-klb'></div>
+                        <div class='plan-klb-descr'></div>
+                    </div>
+                </a>
+                <a href="#!">
+                    <div class='plano'>
+                        <div class='plan-klb'></div>
+                        <div class='plan-klb-descr'></div>
+                    </div>
+                </a>
+                <a href="#!">
+                    <div class='plano'>
+                        <div class='plan-klb'></div>
+                        <div class='plan-klb-descr'></div>
+                    </div>
+                </a>
+
+            </div>
+        </div>
+    </div>
+    <div id='suporte'>
+        <div class='support'>
+            <div class='row'>
+                <div class='col-6 col-md-6 col-sm-12'>
+                    <div class='row'>
+                        <div class='col-md-3 col-sm-12'>
+                            <img src="ico/support.png" alt="" class='suport-img'> 
+                        </div>
+                        <div class='col-md-9 col-sm-12'>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quae ullam corporis cum tempora optio facere, 
+                                pariatur aperiam ad amet eveniet maxime esse saepe ratione animi, velit est molestiae deleniti.</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class='col-6 col-md-6 col-sm-12'>
+                    <div class='row'>
+                        <div class='col-md-3 col-sm-12'>
+                            <img src="ico/working.png" alt="" class='suport-img'>
+                        </div>
+                        <div class='col-md-9 col-sm-12'>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Reiciendis dicta rerum provident id numquam non, odit quibusdam quaerat ratione, cum temporibus ea mollitia?</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <div id='sobre-nos'>
+        <div class='sobre-nos'>
+            <div class='row'>
+                <div class='col-4 col-md-4 col-sm-12'>
+                    <a href="#!">
+                        <img src="image/simcard-show.png" alt="" class='simcard'>
+                    </a>
+                </div>
+                <div class='col-8 col-md-8 col-sm-12'>
+                    <h3><span class='font-color-orange'>Sobre nós</span></h3>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        In magni distinctio iste vero asperiores ratione quas odio hic adipisci, alias ab perferendis explicabo labore ad. 
+                        Eligendi repellat enim autem fugit.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Ipsum, ab aspernatur, laborum non at cum aliquam delectus beatae rerum architecto distinctio omnis, unde ratione odio. 
+                        Exercitationem aliquam officia eius quos.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <div id='form-contato'>
+        <div class='form'>
+            <h3>Entre em <span class='font-color-orange'>contato</span></h3>
+            <form action="">
+                <div class='row'>
+                    <div class='col-6 col-md-6 col-sm-12'>
+                        <div class='row'>
+                            <div class='col-12 col-sm-12'>
+                                <label for="">Nome</label>
+                                <input type="text" class='input-orange'>
+                            </div>
+                            <div class='col-12 col-sm-12'>
+                                <label for="">E-mail</label>
+                                <input type="text" class='input-orange'>
+                            </div>
+                            <div class='col-12 col-sm-12'>
+                                <label for="">Telefone</label>
+                                <input type="text" class='input-orange'>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='col-6 col-md-6 col-sm-12'>
+                            <div class='col-12 col-sm-12'>
+                                <label for="">Assunto</label>
+                                <input type="text" class='input-orange'>
+                            </div>
+                            <div class='col-12 col-sm-12'>
+                                <label for="">Mensagem</label>
+                                <textarea name=""></textarea>
+                            </div>
+                            <div class='col-12 col-sm-12'>
+                                <button class='btn-enviar'>Enviar</button>
+                            </div>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+    <div id='rodape'>
+        <div class='footer-itens row'>
+            <div class='logo-footer'>
+                <a href="#!">
+                    <img src="image/logo-branco.png" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
