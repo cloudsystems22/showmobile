f9bc337679f434ca2b4f786be9de60b8835f95f1
cloudsystems22 <cloudsystems22@gmail.com>
2020-08-30 12:00:39 -0300

    Formulario de contratação

---
+++ contratar.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plano: </title>
+    <link rel="icon" href="ico/1/favicon.png"  sizes="16x16">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/flex.css">
+    <link rel="stylesheet" href="style/pallet.css">
+    <link rel="stylesheet" href="style/style-contratar.css">
+    <script src="https://kit.fontawesome.com/55fcd42dc6.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <section id='head'>
+        <nav class='nav-bar'>
+        <div class='container-nav row'>
+                <a href="#head" class='nav-scroll' class='nav-logo'>
+                    <img src="image/logo-preto.png" alt="Show Mobile" class='logo'>
+                </a>
+              
+                <div class='tel-comercial row'>
+                    <p class=''><span class='tel-span'>0800-2003581</span><br>Atendimento comercial</p>
+                    <ul class='nav-redes' id='navredes'>
+                        <li><a href="#!"><img src="ico/instagram.png" alt="" class='img-ico'></a></li>
+                        <li><a href="#!"><img src="ico/facebook.png" alt="" class='img-ico'></a></li>
+                        <!-- <li><a href="#!"><img src="ico/youtube.png" alt="" class='img-ico-youtube'></a></li> -->
+                    </ul>
+                </div>
+                
+            </div>
+        </nav>
+    </section>
+    <section id='body'>
+        <div class='corpo-contratacao'>
+            <div class='head-contratacao'>
+                <h3>Contratação</h3>
+            </div>
+            <div class='row'>
+                <div class='plano'>
+                    <img src="image/simcard-show.png" alt="" class='simcard'>
+                    <h3>Plano controle 5Gb</h3>
+                    <h4>R$ 49,90</h4>
+                    <hr>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class='form'>
+                   
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+    <section id='footer'>
+        <div class='footer-nav row'>
+            <div class='logo-footer'>
+                <a href="#head" class='nav-scroll'>
+                    <img src="image/logo-branco.png" alt="">
+                </a>
+            </div>
+            <div class='mapa-site'>
+                <p><span>MAPA DO SITE</span></p>
+                <ul class='rodape-list'>
+                    <li><a href="#!">Home</a></li>
+                    <li><a href="#!">Tarifas móveis</a></li>
+                    <li><a href="#!">Nossos artistas</a></li>
+                    <li><a href="#!">Sobre nós</a></li>
+                    <li><a href="#!">Contato</a></li>
+                </ul>
+
+            </div>
+            <div class='contato-footer'>
+                <p><span>CONTATO</span></p>
+                <ul class='rodape-list'>
+                    <li>Email: <span>contato@operadorashow.com.br</span></li>
+                    <li>Telefone: <span>+55(19)3201-2636</span></li>
+                    <li>Comercial: <span>0800-2003581</span></li>
+                    <li>Endereço: <span>Rua Coronel Quirino, 2159</span><br><span>Cambuí - Campinas-SP - 13025-033</span></li>
+                    
+                </ul>
+            </div>
+            <div class='politica-info'>
+                <p><span>POLÍTICA DE INFORMAÇÕES</span></p>
+                <br>
+                <p><a href="https://www.uniglobaltelecom.com/web/files/politica-de-privacidad.pdf" target='_blank'>POLÍTICA DE PRIVACIDADE</a></p>
+                <p><a href="#!">CONDIÇÕES DE DEVOLUÇÃO</a></p>
+                <p><a href="#!">AVISO LEGAL</a></p>
+            </div>
+
+        </div>
+
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <!-- Requisições ajax -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+    <script src="javascript/scroll.js"></script>
+
+</body>
+</html>
